d9fe60382dfed24af46c7e20230d6e5cfd87c461
Jonathan Santos <jonathan01347@gmail.com>
2022-04-19 14:23:41 -0300

    Requisito 03 feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      status: true,
+    };
+  }
+
+  componentDidMount() {
+    this.renderHeader();
+  }
+
+  renderHeader = () => {
+    this.setState({
+      status: true,
+    }, async () => {
+      const pegarGetuser = await getUser();
+      console.log(pegarGetuser);
+      this.setState({
+        status: false,
+        name: pegarGetuser.name,
+      });
+    });
+  }
+
   render() {
+    const { status, name } = this.state;
+    console.log(name.name);
     return (
       <section data-testid="header-component">
-        <h1>olá</h1>
+        <nav data-testid="header-user-name">
+          {status ? <Carregando /> : <h1>{ name }</h1> }
+        </nav>
       </section>
     );
   }
