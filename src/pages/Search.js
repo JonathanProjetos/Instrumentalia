084c6b40fb44f8864953f58c6c2f78c517344d60
Jonathan Santos <jonathan01347@gmail.com>
2022-04-20 01:02:20 -0300

    ajustando o requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Carregando from '../components/Carregando';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       nomeArtista: '',
+      albuns: '',
       btnValidate: true,
       loading: false,
     };
@@ -21,8 +24,17 @@ class Search extends Component {
     });
   }
 
-  handleClick=() => {
+  handleClick= async () => {
+    const { nomeArtista } = this.state;
+    this.setState({
+      loading: true,
+    });
+    const retornoApi = await searchAlbumsAPI(nomeArtista);
 
+    this.setState({
+      loading: false,
+      albuns: retornoApi,
+    });
   }
 
   validateBtn=() => {
@@ -41,32 +53,53 @@ class Search extends Component {
   };
 
   render() {
-    const { btnValidate, loading } = this.state;
+    const { btnValidate, loading, albuns, nomeArtista } = this.state;
+
     return (
-      <div data-testid="page-profile">
-        <Header />
+      <section className="form-login-container">
+        <div data-testid="page-search">
+          <Header />
+          <h1>{`Resultado de álbuns de: ${nomeArtista}`}</h1>
+          {loading ? <Carregando /> : (
 
-        {loading ? <Carregando /> : (
+            <form>
+              <input
+                name="nomeArtista"
+                type="text"
+                data-testid="search-artist-input"
+                onChange={ this.handleChange }
+              />
 
-          <form>
-            <input
-              name="nomeArtista"
-              type="text"
-              data-testid="search-artist-input"
-              onChange={ this.handleChange }
-            />
+              <button
+                type="submit"
+                data-testid="search-artist-button"
+                onClick={ this.handleClick }
+                disabled={ btnValidate }
+              >
+                Pesquisar
+              </button>
+            </form>
+          )}
+        </div>
+        {albuns.length === 0 ? <p>Nenhum álbum foi encontrado</p> : (
+          albuns.map((elm) => (
+            <div key={ elm.artistId }>
+              <div>
+                <p>{ elm.artistName }</p>
+                <img src={ elm.artworkUrl100 } alt="foto da banda" />
+                <p>{ elm.collectionName }</p>
+              </div>
+              <Link
+                data-testid={ `link-to-album-${elm.artistId}` }
+                to="/album/:id"
+              >
+                collectionId
 
-            <button
-              type="button"
-              data-testid="search-artist-button"
-              onClick={ this.handleClick }
-              disabled={ btnValidate }
-            >
-              Pesquisar
-            </button>
-          </form>
+              </Link>
+            </div>
+          ))
         )}
-      </div>
+      </section>
     );
   }
 }
