f5202c95531f32928694b1304dff42a4a3ab1d65
Jonathan Santos <jonathan01347@gmail.com>
2022-04-19 21:23:33 -0300

    requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Carregando from '../components/Carregando';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nomeArtista: '',
+      btnValidate: true,
+      loading: false,
+    };
+  }
+
+  handleChange= ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.validateBtn();
+    });
+  }
+
+  handleClick=() => {
+
+  }
+
+  validateBtn=() => {
+    const { nomeArtista } = this.state;
+    const One = 1;
+
+    if (nomeArtista.length > One) {
+      this.setState({
+        btnValidate: false,
+      });
+    } else {
+      this.setState({
+        btnValidate: true,
+      });
+    }
+  };
+
   render() {
+    const { nomeArtista, btnValidate, loading } = this.state;
     return (
-      <div data-testid="page-search">
+      <div data-testid="page-profile">
         <Header />
+
+        {loading ? <Carregando /> : (
+
+          <form>
+            <input
+              name="nomeArtista"
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              onClick={ this.handleClick }
+              disabled={ btnValidate }
+            >
+              Pesquisar
+            </button>
+          </form>
+        )}
       </div>
     );
   }
