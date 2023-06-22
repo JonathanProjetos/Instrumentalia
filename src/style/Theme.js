e1b3fa3ba5c05dc51a944004e26ef4ab6a322c97
Jonathan Santos <jonathan01347@gmail.com>
2023-06-21 21:07:02 -0300

    feat(Theme)added custom materialUI theme

---
+++ src/style/Theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#a7a9be',
+    },
+    secondary: {
+      main: '#689F38',
+    },
+  },
+  // typography: {
+  //   fontFamily: 'Lobster, cursive',
+  // },
+});
+
+export default theme;
