3286e52aefd478eb5233092e1e5a18fdd95af7b8
Jonathan Santos <jonathan01347@gmail.com>
2023-06-21 21:03:52 -0300

    feat(Palete)create object style

---
+++ src/style/Palete.js
@@ -0,0 +1,33 @@
+export const paleteDark = {
+  background: '#0f0e17',
+  main: '#fffffe',
+  headline: '#0d0d0d',
+  paragraph: '#a7a9be',
+  card: '#8d8d8d',
+  // buttonBorder:
+  buttonText: '#fffffe',
+  buttonBackground: '#ff8906',
+  buttonBackgroundDisabled: '#ffddb7',
+  secundary: '#f25f4c',
+  // secundaryHover:
+  tertiary: '#e53170',
+  highligth: '#ff8906',
+  // extraHover:0d0d0d
+};
+
+export const paleteLight = {
+  background: '#eff0f3',
+  main: '#eff0f3',
+  headline: '#fffffe',
+  paragraph: '#2a2a2a',
+  card: 'white',
+  // buttonBorder:
+  buttonText: '#0d0d0d',
+  buttonBackground: '#ff8e3c',
+  buttonBackgroundDisabled: '#ffddb7',
+  secundary: '#fffffe',
+  // secundaryHover:
+  tertiary: '#d9376e',
+  highligth: '#ff8e3c',
+  // extraHover:
+};
